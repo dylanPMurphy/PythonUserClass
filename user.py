9514740ddbfc21301f32962cb1eae2a5bfb233da
dylanPMurphy <dylanmurphydba@gmail.com>
2020-09-02 21:36:15 -0700

    methods done now testing

--- user.py
+++ user.py
@@ -13,9 +13,10 @@ class User:		# here's what we have so far
 #For this assignment, we'll add some functionality to the User class:
 
     def make_withdrawal(self, amount):                           ## this method decrease the user's balance by the amount specified
-        pass
+        self.account_balance -= amount
     def display_user_balance(self):                              #- have this method print the user's name and account balance to the terminal
-        pass                                                    #eg. "User: Guido van Rossum, Balance: $150
+        print("User:",self.name,",","Balance:",str(self.account_balance))                                                   #eg. "User: Guido van Rossum, Balance: $150
 
     def transfer_money(self, other_user, amount):                # - have this method decrease the user's balance by the amount and add that amount to other other_user's balance
-        pass
+        self.make_withdrawal(amount)
+        other_user.make_deposit(amount)
